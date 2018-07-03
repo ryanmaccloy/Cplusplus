c37f2e7bd21906be06b41d4f0b7bbaf03de972d7
Ryan MacCloy <ryan5190@yahoo.com>
2018-07-03 12:56:31 -0400

    Which number is greater?
    Beginner C++

---
+++ WhichNumberIsGreater.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int first,second;
+cout<<"Enter two Numbers"<<endl;
+cin>>first>>second;
+if (first>second)
+{
+cout<<"First number is greater which is="<<first<<endl;
+}
+else
+cout<<"Second number is greater which is="<<second<<endl;
+return 0;
+}
